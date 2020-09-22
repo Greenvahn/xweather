edd5107e47530f34d17c24e35882fc7ecbc57085
Greenvahn <saulverdemartinez@gmail.com>
2020-09-22 22:45:55 +0100

    WA-23 - update time dynamically

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <weather-page :data-API="data" />
+  <weather-page :data-API="data" @click="stopTimer()"/>
 </template>
 <script>
 import weatherPage from "./components/weatherPage.vue";
@@ -15,50 +15,73 @@ export default {
       data: {
         currentWeather: null,
         foreCast: [],
-        location: null
+        location: null,
+        timer: "",
       },
       city: "London",
       units: "metric",
-      API_KEY: "07dd487fb51c8fd64b7c189f26dcffa5"
+      API_KEY: "07dd487fb51c8fd64b7c189f26dcffa5",
     };
   },
   created() {
     this.getCurrentWeather();
     this.foreCast();
     this.generateDataButton();
+    //this.location = this.timer;
+  },
+  mounted() {
+    // const newDataButton = new geoWidget(this.city);
+    // this.data.location = window.setInterval(newDataButton.getData(), 1000);
   },
   methods: {
     // FECTH API
     // ===========================================
     // Get current weather - today
-    async getCurrentWeather(){
-      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=${this.units}&APPID=${this.API_KEY}`);
+    async getCurrentWeather() {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=${this.units}&APPID=${this.API_KEY}`
+      );
       const data = await res.json();
       this.data.currentWeather = data;
       // console.log("API Data -->", this.data.currentWeather)
     },
     // Get forecast for next 4 days
-    async foreCast(){
-      const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${this.city}&units=${this.units}&exclude=hourly,daily&APPID=${this.API_KEY}`);
+    async foreCast() {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${this.city}&units=${this.units}&exclude=hourly,daily&APPID=${this.API_KEY}`
+      );
       const data = await res.json();
 
       // Distribute the original list to have only 4 days - we stop array iteration at 24 ( 4th Day)
       // The endpoint gets the weather status each day at 15:00pm
       let sum = 0;
-      data.list.forEach((item,index) => {
+      data.list.forEach((item, index) => {
         if (sum === index && sum <= 24) {
           this.data.foreCast.push(item);
-          sum +=8;
+          sum += 8;
         }
-      })
+      });
       // console.log("API Data -->", this.data.foreCast)
     },
 
-    generateDataButton () {
+    generateDataButton() {
+      // Generate initial Data button
       const newDataButton = new geoWidget(this.city);
       this.data.location = newDataButton.getData();
-    }
 
+      // Initi timer update
+      this.updateTime();
+    },
+
+    updateTime() {
+      setInterval(() => {
+        const newDataButton = new geoWidget(this.city);
+        this.data.location.time = newDataButton.getTime();
+      }, 1000);
+    },
+    stopTimer() {
+      clearInterval(this.data.location.time);
+    }
   },
 };
 </script>
