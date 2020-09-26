052d293a322ea3f6c180923f0d631e6104718cd6
Greenvahn <saulverdemartinez@gmail.com>
2020-09-26 15:37:04 +0100

    WA-18 - fetch API refactor

---
+++ src/composables/fetchData.js
@@ -0,0 +1,43 @@
+export default class fetchData {
+  // Retrieve API_KEY
+  constructor(city, units){
+      this.city = city;
+      this.units = units;
+      this.key = '07dd487fb51c8fd64b7c189f26dcffa5';
+  }
+
+  // FECTH API
+  // ===========================================
+  // Get current weather - today
+  getCurrentWeather = async () => {
+    console.log("API_KEY", this.key);
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=${this.units}&APPID=${this.key}`
+    );
+    const data = await res.json();
+    // this.data.currentWeather = data;
+    // console.log("API Data -->", this.data.currentWeather)
+    return data;
+  };
+  // Get forecast for next 4 days
+  getforeCast = async () => {
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${this.city}&units=${this.units}&exclude=hourly,daily&APPID=${this.key}`
+    );
+    const data = await res.json();
+
+    // Distribute the original list to have only 4 days - we stop array iteration at 24 ( 4th Day)
+    // The endpoint gets the weather status each day at 15:00pm
+    let sum = 0;
+    const foreCastOutput= [];
+    data.list.forEach((item, index) => {
+      if (sum === index && sum <= 24) {
+        //this.data.foreCast.push(item);
+        foreCastOutput.push(item)
+        sum += 8;
+      }
+    });
+
+    return foreCastOutput;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 
 import { createApp } from "vue";
 import App from "./App.vue";
+require('dotenv').config();
 
 const app = createApp(App);
 app.use().mount("#app");
